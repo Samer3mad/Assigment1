6d31e71d6a51f2e3682db89bf7af244b89059d21
Samer Zaki <Samer.Zaki22@svnu.edu.eg>
2024-12-12 08:57:39 +0200

    Add project files.

---
+++ Assigment/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Assigment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Question 01
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(num);
+            #endregion
+
+            #region Question 2
+            //int x = 10;
+            //Console.WriteLine(x);
+
+            //int y = x;
+            //Console.WriteLine(y);
+
+            //y = 20;
+            //Console.WriteLine(y);
+            #endregion
+
+            #region Question 03
+            //Point p1;
+            //p1 = new Point();
+
+            //Point p2;
+            //p2 = new Point();
+
+            //p2 = p1;
+
+            //object obj1 = new object();
+            //object obj2 = new object();
+            //obj1 = "Samer";
+            //obj2 = "Emad";
+
+            //obj2 = obj1;
+            //Console.WriteLine(obj2);
+
+            #endregion
+        }
+    }
+}
